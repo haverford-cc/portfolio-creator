f75b2f51373de49287847b138497cf88e8c27d79
Den4200 <dennis@dennispham.me>
2021-08-08 02:20:16 -0400

    Proxy `/api` to the backend API.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/:path*",
+      },
+    ];
+  },
+};
