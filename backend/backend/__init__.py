63f6ca5da0aef59063ef41efab450090a79e8123
Den4200 <dennis@dennispham.me>
2021-08-11 21:40:58 -0400

    Implement CORS for the backend.

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from backend.constants import ALLOW_ORIGINS, ALLOW_ORIGIN_REGEX
 from backend.routes import routers
 
 app = FastAPI(
     docs_url=None,
     redoc_url=None,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOW_ORIGINS,
+    allow_origin_regex=ALLOW_ORIGIN_REGEX,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
 )
 
 for router in routers:
--- backend/backend/constants.py
+++ backend/backend/constants.py
@@ -3,3 +3,18 @@ from decouple import config
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 DATABASE_URL = config("DATABASE_URL")
+
+if DEBUG:
+    ALLOW_ORIGINS = [
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+    ]
+    ALLOW_ORIGIN_REGEX = None
+
+else:
+    ALLOW_ORIGINS = [
+        "https://portfolio-creator.dens.dev",
+        "https://portfolio-creator.vercel.app",
+        "https://portfolio-creator-haverford-cc.vercel.app",
+    ]
+    ALLOW_ORIGIN_REGEX = r"https://portfolio-creator-git-.*-haverford-cc\.vercel\.app"
