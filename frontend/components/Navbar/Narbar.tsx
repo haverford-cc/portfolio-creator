1e46d69ca3b3b04969b4998c5af594d69e2ced42
John Murphy <67712161+jmurphy5613@users.noreply.github.com>
2021-08-12 11:10:44 -0400

    Update frontend/components/Navbar/Narbar.tsx
    Co-authored-by: Dennis Pham <dennis@dennispham.me>

--- frontend/components/Navbar/Narbar.tsx
+++ frontend/components/Navbar/Narbar.tsx
@@ -2,33 +2,31 @@ import Link from "next/link";
 import styles from "./navbar.module.css";
 import Head from "next/head";
 
-function Navbar() {
-  return (
-    <div className={styles.background}>
-      <div className={styles.logo}>
-        <h1 className={styles.title}>Portfolio Creator</h1>
-      </div>
-      <div className={styles.middle}>
-        <ul className={styles.navlist}>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/portfolio">Templates</Link>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.right}>
-        <a href="" className={styles.signin}>
-          Sign in
-        </a>
-        <button className={styles.signup}>Sign Up</button>
-      </div>
+const Navbar = () => (
+  <div className={styles.background}>
+    <div className={styles.logo}>
+      <h1 className={styles.title}>Portfolio Creator</h1>
     </div>
-  );
-}
+    <div className={styles.middle}>
+      <ul className={styles.navlist}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Templates</Link>
+        </li>
+      </ul>
+    </div>
+    <div className={styles.right}>
+      <a href="" className={styles.signin}>
+        Sign in
+      </a>
+      <button className={styles.signup}>Sign Up</button>
+    </div>
+  </div>
+);
 
 export default Navbar;
