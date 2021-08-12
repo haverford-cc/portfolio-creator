63f1ce6697586ee6afaeeb5198148238a4e02cec
John Murphy <jmurphy5613@gmail.com>
2021-08-11 22:31:35 -0400

    added navbar component and fetched upstream

---
+++ frontend/components/Navbar/Narbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import styles from './navbar.module.css';
+import Head from 'next/head';
+
+function Navbar() {
+    return(
+        <div className={styles.background}>
+            <div className={styles.logo}>
+                <h1 className={styles.title}>Portfolio Creator</h1>
+            </div>
+            <div className={styles.middle}>
+                <ul className={styles.navlist}>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/portfolio">Templates</Link></li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+                <Link href="/login">Sign In</Link>
+                <button className={styles.signup}>Sign Up</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar/Narbar";
 
 const Home = () => (
   <div className={styles.container}>
+    <Navbar />
     <Head>
     </Head>
   </div>
