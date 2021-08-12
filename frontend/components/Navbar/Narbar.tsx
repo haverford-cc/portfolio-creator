2f94891fba8581a0e81b5d459ceb5ee6c21eb73d
John Murphy <jmurphy5613@gmail.com>
2021-08-12 16:16:54 -0400

    added prettier

--- frontend/components/Navbar/Narbar.tsx
+++ frontend/components/Navbar/Narbar.tsx
@@ -3,18 +3,18 @@ import styles from "./navbar.module.css";
 
 const Navbar = () => (
   <div className={styles.background}>
-      <h1 className={styles.title}>Portfolio Creator</h1>
-      <ul className={styles.navlist}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/portfolio">Templates</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
+    <h1 className={styles.title}>Portfolio Creator</h1>
+    <ul className={styles.navlist}>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/portfolio">Templates</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+    </ul>
     <div>
       <a href="" className={styles.signin}>
         Sign in
