e345595ced2f82a6ca270842fdc6e8a533ce4069
Den4200 <dennis@dennispham.me>
2021-08-12 22:15:18 -0400

    Create `NewUser` and `User` pydantic schemas.

---
+++ backend/backend/schemas/auth.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, validator
+
+PASSWORD_MIN_LENGTH = 8
+
+
+class NewUser(BaseModel):
+    """Represents a new user."""
+    email: EmailStr
+
+    password1: str
+    password2: str
+
+    @validator("password1")
+    def password_length(cls, p1) -> str:
+        """Ensure password is at least 8 characters long."""
+        if len(p1) < PASSWORD_MIN_LENGTH:
+            raise ValueError("Password is too short")
+
+        return p1
+
+    @validator("password2")
+    def passwords_match(cls, p2, values):
+        """Ensure both passwords match."""
+        if "password1" in values and values["password1"] != p2:
+            raise ValueError("Passwords do not match")
+
+        return p2
+
+
+class User(BaseModel):
+    """Represents a user."""
+    email: EmailStr
+    password: str
