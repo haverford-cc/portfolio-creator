886fba5e8de68c3b75c7cf2cb0deedf1b87dc436
Den4200 <dennis@dennispham.me>
2021-08-12 22:17:34 -0400

    Create `signup` and `login` functions.

--- backend/backend/utils/auth.py
+++ backend/backend/utils/auth.py
@@ -37,3 +37,65 @@ class JWTBearer(APIKeyCookie):
 
         request.state.user_id = token_data["id"]
         return token
+
+
+async def signup(db_session: AsyncSession, user: UserSchema) -> int:
+    """Create an account for a new user."""
+    async with db_session() as session:
+        async with session.begin():
+            db_user = await session.execute(
+                select(User).where(User.email == user.email)
+            )
+            if db_user.first() is not None:
+                raise HTTPException(status_code=HTTP_409_CONFLICT, detail="User already exists.")
+
+            loop = asyncio.get_running_loop()
+            with ThreadPoolExecutor() as pool:
+                password_hash = await loop.run_in_executor(
+                    pool,
+                    bcrypt.hash,
+                    user.password1,
+                )
+
+            db_user = User(email=user.email, password_hash=password_hash)
+            session.add(db_user)
+            await session.flush()
+
+            return db_user.id
+
+
+async def login(db_session: AsyncSession, user: UserSchema) -> str:
+    """Login the given user."""
+    async with db_session() as session:
+        db_user = (
+            await session.execute(
+                select(User).where(User.email == user.email)
+            )
+        ).scalars().first()
+
+        if db_user is None:
+            raise HTTPException(
+                status_code=HTTP_401_UNAUTHORIZED,
+                detail="Invalid email or password"
+            )
+
+        loop = asyncio.get_running_loop()
+        with ThreadPoolExecutor() as pool:
+            if await loop.run_in_executor(
+                pool,
+                bcrypt.verify,
+                user.password,
+                db_user.password_hash
+            ):
+                epoch_time = int(time.time())
+                payload = {
+                    "id": db_user.id,
+                    "iat": epoch_time,
+                    "exp": epoch_time + 60 * 60 * 24 * 7,  # 1 week later
+                }
+                return jwt.encode(payload, JWT_SECRET)
+
+    raise HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Invalid email or password"
+    )
