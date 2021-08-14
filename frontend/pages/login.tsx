2e46ec2332db31ec705cc1ef99f979e07d84dd07
John <jmurphy5613@gmail.com>
2021-08-14 17:24:41 +0000

    adeded content and html

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,9 +1,22 @@
 import styles from '../styles/login.module.css';
 
 const Login = () => {
-    <div className={styles.background}>
-
-    </div>
+    return(
+        <div className={styles.background}>
+            <div className={styles.login_form}>
+                <div className={styles.greeting}>
+                    <h1 className={styles.title}>Welcome back to <span>Portfolio Creator</span></h1>
+                    <p className={styles.description}>Login to access your saved websites!</p>
+                </div>
+                <div className={styles.login_inputs}>
+                    <input type="text" placeholder="Email"/>
+                    <br />
+                    <input type="text" placeholder="Password"/>
+                </div>
+                <button className={styles.login}>Login</button>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
