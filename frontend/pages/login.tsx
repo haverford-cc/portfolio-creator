ae0d223c7fbc7a746377fcaa86fc6b9db926c7a2
John <jmurphy5613@gmail.com>
2021-08-14 17:59:14 +0000

    added formik form and Yup schema

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,20 +1,44 @@
 import styles from '../styles/login.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import Navbar from '../components/Navbar/Narbar';
 
 const Login = () => {
+
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+
+    const formSchema = Yup.object().shape({
+        email: Yup.string().required('Required'),
+        password: Yup.string().required('Required')
+    })
+
+    const formSubmission = (values) => {
+
+    }
+
     return(
         <div className={styles.background}>
-            <div className={styles.login_form}>
-                <div className={styles.greeting}>
-                    <h1 className={styles.title}>Welcome back to <span>Portfolio Creator</span></h1>
-                    <p className={styles.description}>Login to access your saved websites!</p>
-                </div>
-                <div className={styles.login_inputs}>
-                    <input type="text" placeholder="Email"/>
-                    <br />
-                    <input type="text" placeholder="Password"/>
-                </div>
-                <button className={styles.login}>Login</button>
-            </div>
+            <Navbar />
+            <Formik initialValues={initialValues} onSubmit={formSubmission} validationSchema={formSchema} className={styles.login_form}>
+                <Form className={styles.login_form}>
+                    <div className={styles.greeting}>
+                        <h1 className={styles.title}>Welcome back to <span>Portfolio Creator</span></h1>
+                        <p className={styles.description}>Login to access your saved websites!</p>
+                    </div>
+                    <div className={styles.login_inputs}>
+                        <label>Username</label>
+                        <ErrorMessage name="email" component="span"/>
+                        <Field className={styles.input} type="text" name="email" placeholder="Email" />
+                        <label>Password</label>
+                        <ErrorMessage name="password" component="span"/>
+                        <Field className={styles.input} type="password" name="password" placeholder="Password" />
+                    </div>
+                    <button className={styles.login}>Login</button>
+                </Form>
+            </Formik>
         </div>
     )
 }
