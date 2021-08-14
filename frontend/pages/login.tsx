fc0627e205e2f31830eb094a84abc404efd4b3ee
John <jmurphy5613@gmail.com>
2021-08-14 18:36:00 +0000

    finished logifrontend

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -11,8 +11,8 @@ const Login = () => {
     }
 
     const formSchema = Yup.object().shape({
-        email: Yup.string().required('Required'),
-        password: Yup.string().required('Required')
+        email: Yup.string().required(' Required'),
+        password: Yup.string().required(' Required')
     })
 
     const formSubmission = (values) => {
@@ -20,26 +20,26 @@ const Login = () => {
     }
 
     return(
-        <div className={styles.background}>
-            <Navbar />
-            <Formik initialValues={initialValues} onSubmit={formSubmission} validationSchema={formSchema} className={styles.login_form}>
-                <Form className={styles.login_form}>
-                    <div className={styles.greeting}>
-                        <h1 className={styles.title}>Welcome back to <span>Portfolio Creator</span></h1>
-                        <p className={styles.description}>Login to access your saved websites!</p>
-                    </div>
-                    <div className={styles.login_inputs}>
-                        <label>Username</label>
-                        <ErrorMessage name="email" component="span"/>
-                        <Field className={styles.input} type="text" name="email" placeholder="Email" />
-                        <label>Password</label>
-                        <ErrorMessage name="password" component="span"/>
-                        <Field className={styles.input} type="password" name="password" placeholder="Password" />
-                    </div>
-                    <button className={styles.login}>Login</button>
-                </Form>
-            </Formik>
-        </div>
+            <div className={styles.background}>
+                <Navbar />
+                <Formik initialValues={initialValues} onSubmit={formSubmission} validationSchema={formSchema} className={styles.login_form}>
+                    <Form className={styles.login_form}>
+                        <div className={styles.greeting}>
+                            <h1 className={styles.title}>Welcome back to <span>Portfolio Creator</span></h1>
+                            <p className={styles.description}>Login to access your saved websites!</p>
+                        </div>
+                        <div className={styles.login_inputs}>
+                            <label>Username</label>
+                            <ErrorMessage name="email">{msg => <span className={styles.span}>{msg}</span>}</ErrorMessage>
+                            <Field className={styles.input} type="text" name="email" placeholder="Email" />
+                            <label>Password</label>
+                            <ErrorMessage name="password">{msg => <span className={styles.span}>{msg}</span>}</ErrorMessage>
+                            <Field className={styles.input} type="password" name="password" placeholder="Password" />
+                        </div>
+                        <button className={styles.login}>Login</button>
+                    </Form>
+                </Formik>
+            </div>
     )
 }
 
