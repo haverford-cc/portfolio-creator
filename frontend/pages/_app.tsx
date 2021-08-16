aacd86809796b5023d972ba65c6480dee66ea7e3
John <jmurphy5613@gmail.com>
2021-08-16 20:16:12 +0000

    got rid of api context

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,20 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import * as axios from 'axios';
-import { ApiContext } from '../helpers/ApiContext';
+import axios from 'axios';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  
-  const { ApiClient } = (axios as any).create({
-    baseURL: "/api",
-    withCredentials: true,
-    // headers: { "X-Forwarded-Host":  },
-  });
 
   return (
-    <ApiContext.Provider value={ ApiClient }>
-      <Component {...pageProps} />
-    </ApiContext.Provider> 
+    <Component {...pageProps} />
   )
 };
 
